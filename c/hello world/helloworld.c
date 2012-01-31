1010323d76470a9232eca433d60a76f3e7217b04
Steve King <steve@sming.com.au>
2012-01-31 23:25:35 +1100

    Adding a C example of "Hello World"

---
+++ c/hello world/helloworld.c
@@ -0,0 +1,13 @@
+/* Simple Hello World
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main (void);
+
+int main() {
+	char nameMine[] = "Borris";
+	printf("Hello, my name is %s\r\n",nameMine);
+	return 0;
+}
